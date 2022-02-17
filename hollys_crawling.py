e882026c8b837299ba1fa98643f9baf060b204d3
pjoonyoung <ekskrk258@naver.com>
2022-02-17 14:33:16 +0900

    0217

--- hollys_crawling.py
+++ hollys_crawling.py
@@ -1,9 +1,12 @@
 from bs4 import BeautifulSoup
 import urllib.request
+import pandas as pd
 
 result = [] # 작업결과를 저장할 빈 리스트 선언
-
-for page in range(54,55):
+end_flag = 0
+for page in range(1,100):
+    if end_flag == 1:
+        break
     hollys_url = 'https://www.hollys.co.kr/store/korea/korStore2.do?pageNo=%d&sido=&gugun=&store=' %page
     print(hollys_url)
     hollys_html = urllib.request.urlopen(hollys_url)
@@ -13,9 +16,10 @@ for page in range(54,55):
     tag_tr = tag_tbody.find_all('tr')
     # print(tag_tr)
     for store in tag_tr: # 각 페이지당 10개의 대리점 정보가 store 저장되어 추출
-        print(len(store))
+        if len(store) == 3:
+            end_flag = 1
+            break
         store_td = store.find_all('td') # 각 tr 내의 td 정보를 리스트로 추출하여 저장
-        print(len(store_td))
         store_sido = store_td[0].string # 대리점의 시,도 추출
         store_name = store_td[1].string # 대리점의 이름 추출
         store_address = store_td[3].string # 대리점의 주소 추출
@@ -23,4 +27,6 @@ for page in range(54,55):
 
         result.append([store_name]+[store_sido]+[store_address]+[store_phone])
 
-    # print(result)
+hollys_df = pd.DataFrame(result, columns=('대리점이름','대리점시도','대리점주소','전화번호'))
+
+hollys_df.to_csv('할리스대리점정보.csv', encoding='cp949', mode='w', index=True)
